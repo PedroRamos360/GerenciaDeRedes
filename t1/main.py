95f7e987c853988a41080d5c10eb2017d313cfce
Pedro Ramos <pedrowarkenramos@gmail.com>
2023-09-28 08:57:38 -0300

    inputs for more customizable script

--- t1/main.py
+++ t1/main.py
@@ -5,6 +5,8 @@ from get_device_info import get_device_info
 from get_manufacturer import load_oui_database, get_mac_manufacturer
 
 oui_database = load_oui_database()
+timeout = 5
+max_workers = 10
 
 def get_all_ips_in_network(network_cidr):
     try:
@@ -14,28 +16,33 @@ def get_all_ips_in_network(network_cidr):
     except ValueError as e:
         return str(e)
 
-def ping_and_print_info(ip):
-    response = ping(ip, 1)
+
+def ping_and_print_info(ip, timeout):
+    response = ping(ip, timeout)
     if response is not None and response is not False:
-        for i in range(3):
-            device_info = get_device_info(ip)
-            if device_info is not None:
-                print(
-                    f"Host: {ip} is up MAC: {device_info['mac']} Vendor: {get_mac_manufacturer(device_info['mac'], oui_database)} is router: {device_info['is_router']}"
-                )
-                break
-            else:
-                print(f"Host: {ip} is up can't get device information, trying again...")
+        device_info = get_device_info(ip)
+        if device_info is not None:
+            print(
+                f"Host: {ip} is up MAC: {device_info['mac']} Vendor: {get_mac_manufacturer(device_info['mac'], oui_database)}"
+            )
+        else:
+            print(f"Host: {ip} is up can't get device information")
     else:
         print(f"Host: {ip} is down")
 
+
 if __name__ == "__main__":
     network_cidr = input("Enter network CIDR: ")
+    timeout_input = input("Set timeout for pings (default=5): ")
+    if (timeout_input.strip() != ""):
+        timeout = int(timeout_input)
+    max_workers_input = input ("Set max workers (default=10): ")
+    if (max_workers_input.strip() != ""):
+        max_workers = int(max_workers_input)
     ips_in_network = get_all_ips_in_network(network_cidr)
 
-    
-    with ThreadPoolExecutor(max_workers=10) as executor:  
-        futures = [executor.submit(ping_and_print_info, ip) for ip in ips_in_network]
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        futures = [executor.submit(ping_and_print_info, ip, timeout) for ip in ips_in_network]
 
     for future in futures:
         future.result()
