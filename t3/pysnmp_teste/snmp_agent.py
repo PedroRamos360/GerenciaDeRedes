07a55371ccb8b32ac6f6e3e6bb38145d83595809
JordanoXavier <112017191+JordanoXavier@users.noreply.github.com>
2023-12-03 19:45:24 -0300

    mib name change

--- t3/pysnmp_teste/snmp_agent.py
+++ t3/pysnmp_teste/snmp_agent.py
@@ -29,7 +29,7 @@ config.addVacmUser(snmpEngine,
 
 
 snmpContext = context.SnmpContext(snmpEngine)
-logging.debug('Loading __EXAMPLE-MIB module...'),
+logging.debug('Loading __DISCOVERY-MIB module...'),
 mibBuilder = snmpContext.getMibInstrum().getMibBuilder()
 (MibTable,
  MibTableRow,
@@ -41,33 +41,27 @@ mibBuilder = snmpContext.getMibInstrum().getMibBuilder()
     'MibTableColumn',
     'MibScalarInstance'
 )
-logging.debug('done')
+# logging.debug('done')
 
 
 RowStatus, = mibBuilder.importSymbols('SNMPv2-TC', 'RowStatus')
 
 mibBuilder.exportSymbols(
-    '__EXAMPLE-MIB',
-    # table object
+    '__DISCOVERY-MIB',
     exampleTable=MibTable((1, 3, 6, 4, 1)).setMaxAccess('readcreate'),
-    # table row object, also carries references to table indices
-    exampleTableEntry=MibTableRow((1, 3, 6, 4, 1, 5)).setMaxAccess('readcreate').setIndexNames((0, '__EXAMPLE-MIB', 'exampleTableColumn1')),
-    # table column: string index
+    exampleTableEntry=MibTableRow((1, 3, 6, 4, 1, 5)).setMaxAccess('readcreate').setIndexNames((0, '__DISCOVERY-MIB', 'exampleTableColumn1')),
     exampleTableColumn1=MibTableColumn((1, 3, 6, 4, 1, 5, 1), v2c.OctetString()).setMaxAccess('readcreate'),
-    # table column: string value
     exampleTableColumn2=MibTableColumn((1, 3, 6, 4, 1, 5, 2), v2c.IpAddress()).setMaxAccess('readcreate'),
-    # table column: integer value with default
     exampleTableColumn3=MibTableColumn((1, 3, 6, 4, 1, 5, 3), v2c.OctetString()).setMaxAccess('readcreate'),
-    # table column: row status
     exampleTableStatus=MibTableColumn((1, 3, 6, 4, 1, 5, 4), v2c.OctetString()).setMaxAccess('readcreate')
 )
-logging.debug('done')
+# logging.debug('done')
 
 (exampleTableEntry,
  exampleTableColumn2,
  exampleTableColumn3,
  exampleTableStatus) = mibBuilder.importSymbols(
-    '__EXAMPLE-MIB',
+    '__DISCOVERY-MIB',
     'exampleTableEntry',
     'exampleTableColumn2',
     'exampleTableColumn3',
@@ -82,8 +76,11 @@ mibInstrumentation.writeVars(
      (exampleTableStatus.name + rowInstanceId, 'Fabricante XYZ'))
 )
 
-logging.debug('done')
-logging.debug('Snmp Agent Start')
+logging.debug('Snmp Agent Start \n')
+
+logging.debug("IP:192.168.1.1")
+logging.debug("MAC: 00:1A:2B:3C:4D:5E")
+logging.debug("Fabricante: XYZ")
 
 cmdrsp.GetCommandResponder(snmpEngine, snmpContext)
 cmdrsp.SetCommandResponder(snmpEngine, snmpContext)
